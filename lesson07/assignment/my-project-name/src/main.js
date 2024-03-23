88783c1b120b40bb1d6dc081aaed3d03313265ce
michael-b-palmer <104705856+michael-b-palmer@users.noreply.github.com>
2024-03-23 14:39:26 -0700

    make a pr

--- lesson07/assignment/my-project-name/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import router from './router'; // Ensure this path matches your project structure
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
